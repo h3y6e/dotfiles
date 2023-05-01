a436ff4bb0519e7604bca9d5e6fdad850d347a78
heyhoe <h3y6e@pm.me>
2023-05-01 14:15:01 +0900

    update sys config

--- .chezmoiscripts/darwin/run_onchange_after_configure.sh
+++ .chezmoiscripts/darwin/run_onchange_after_configure.sh
@@ -8,13 +8,27 @@ set -Eeufo pipefail
 # Dockを自動的に表示/非表示
 defaults write com.apple.dock autohide -bool true
 # リピート入力認識までの時間: 最短
-defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write -g InitialKeyRepeat -int 15
 # キーのリピート: 最速
-defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write -g KeyRepeat -int 2
 # キー長押しによる連続入力
 defaults write -g ApplePressAndHoldEnabled -bool false
+# 外部キーボードのF1, F2などのキーを標準のファンクションキーとして使用
+defaults write -g com.apple.keyboard.fnState -bool true
+# キーボードショートカット > 次のウィンドウを操作対象にする: オフ (ブラウザのcmd+2と干渉する為)
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 27 "{enabled=0;value={parameters=(64,33,1048576);type='standard';};}"
+# キーボードショートカット > Spotlight検索を表示: オフ
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 64 "{enabled=0;value={parameters=(65535,49,1048576);type='standard';};}"
 # トラックパッド > クリック: 弱い
 defaults write com.apple.AppleMultitouchTrackpad FirstClickThreshold -int 0
 defaults write com.apple.AppleMultitouchTrackpad SecondClickThreshold -int 0
 # トラックパッド > 軌跡の速さ: 真ん中
-defaults write 'Apple Global Domain' com.apple.trackpad.scaling -float 1
+defaults write -g com.apple.trackpad.scaling -float 1
+# 起動時にサウンドを再生: オフ
+sudo nvram SystemAudioVolume=" "
+# ユーザインターフェイスのサウンドエフェクトを再生: オフ
+defaults write com.apple.systemsound com.apple.sound.uiaudio.enabled -bool false
+# 音量を変更する時にフィードバックを再生: オン
+defaults write -g com.apple.sound.beep.feedback -bool true
+# デスクトップ項目: オン
+defaults write com.apple.finder CreateDesktop -bool true
