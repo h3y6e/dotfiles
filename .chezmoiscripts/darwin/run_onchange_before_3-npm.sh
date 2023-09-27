ff17ad9eb162ce123d657bea12a30069c24d4ea4
heyhoe <h3y6e@pm.me>
2023-09-28 02:38:40 +0900

    add bun

--- .chezmoiscripts/darwin/run_onchange_before_3-npm.sh
+++ .chezmoiscripts/darwin/run_onchange_before_3-npm.sh
@@ -6,11 +6,11 @@ printf "\033[3;35m%s\033[m\n" "npm installing..."
 
 if ! type github-copilot-cli &>/dev/null; then
   echo "installing github-copilot-cli..."
-  npm install -g @githubnext/github-copilot-cli
-  github-copilot-cli auth
+  bun install -g @githubnext/github-copilot-cli
+  "$HOME/.bun/bin/github-copilot-cli" auth
 fi
 
 if ! type npm-check &>/dev/null; then
   echo "installing npm-check..."
-  npm install -g npm-check
+  bun install -g npm-check
 fi
