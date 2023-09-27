e8e1bbbce4928fd52ae6715110dcb65a1758dcdf
heyhoe <h3y6e@pm.me>
2023-09-28 02:48:30 +0900

    use sudo_local
    ref: https://social.camph.net/notes/01HBBTMT9JC58H2K34CGP18RSX

--- .chezmoiscripts/darwin/run_before_pam.sh
+++ .chezmoiscripts/darwin/run_before_pam.sh
@@ -2,10 +2,8 @@
 
 set -Eeufo pipefail
 
-file="/etc/pam.d/sudo"
-if ! grep 'pam_tid.so' ${file} > /dev/null; then
+file="/etc/pam.d/sudo_local"
+if [[ ! -f ${file} ]]; then
   printf "\033[3;35m%s\033[m\n" "setting up pam..."
-  sudo sed -i "" '2i\
-auth       sufficient     pam_tid.so # add by chezmoiscripts
-  ' ${file}
+  echo "auth sufficient pam_tid.so" | sudo tee ${file}
 fi
