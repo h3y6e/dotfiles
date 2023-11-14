5cfdc906ec605f1c71bac9582f210ca4f2ccf4c8
heyhoe <h3y6e@pm.me>
2023-11-15 03:21:10 +0900

    Add aliases

--- Documents/WindowsPowerShell/Profile.ps1
+++ Documents/WindowsPowerShell/Profile.ps1
@@ -1,5 +1,17 @@
 Set-PSReadLineOption -EditMode Emacs
 
+# chezmoi
+Set-Alias cz chezmoi
+function czc { code -n $(chezmoi source-path) }
+
+# ls
+function l { eza -a }
+function ll { eza -lh }
+function la { eza -lha }
+
+# git
+Set-Alias gti git
+
 # ghq
 if ((Get-Command ghq -ErrorAction SilentlyContinue) -and (Get-Command peco -ErrorAction SilentlyContinue)) {
     function g {
