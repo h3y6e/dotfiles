b6f80fd0d8304a51d35ba93cccae64580c7e969d
heyhoe <h3y6e@pm.me>
2023-11-15 03:27:08 +0900

    fix

--- Documents/WindowsPowerShell/Profile.ps1
+++ Documents/WindowsPowerShell/Profile.ps1
@@ -1,34 +1,38 @@
 Set-PSReadLineOption -EditMode Emacs
 
 # chezmoi
-Set-Alias cz chezmoi
-function czc { code -n $(chezmoi source-path) }
+if (Get-Command chezmoi -ErrorAction SilentlyContinue) {
+  Set-Alias cz chezmoi
+  function czc { code -n $(chezmoi source-path) }
+}
 
-# ls
-function l { eza -a }
-function ll { eza -lh }
-function la { eza -lha }
+# eza
+if (Get-Command eza -ErrorAction SilentlyContinue) {
+  function l { eza -a }
+  function ll { eza -lh }
+  function la { eza -lha }
+}
 
 # git
 Set-Alias gti git
 
 # ghq
 if ((Get-Command ghq -ErrorAction SilentlyContinue) -and (Get-Command peco -ErrorAction SilentlyContinue)) {
-    function g {
-        $dir = ghq list | peco
-        if (![string]::IsNullOrEmpty($dir)) {
-            $path = (ghq root) + "/$dir"
-            Set-Location -LiteralPath $path
-        }
+  function g {
+    $dir = ghq list | peco
+    if (![string]::IsNullOrEmpty($dir)) {
+      $path = (ghq root) + "/$dir"
+      Set-Location -LiteralPath $path
     }
+  }
 
-    if (Get-Command code -ErrorAction SilentlyContinue) {
-        function c {
-            $dir = ghq list | peco
-            if (![string]::IsNullOrEmpty($dir)) {
-                $path = (ghq root) + "/$dir"
-                code -n $path
-            }
-        }
+  if (Get-Command code -ErrorAction SilentlyContinue) {
+    function c {
+      $dir = ghq list | peco
+      if (![string]::IsNullOrEmpty($dir)) {
+        $path = (ghq root) + "/$dir"
+        code -n $path
     }
+    }
+  }
 }
