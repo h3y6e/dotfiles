2edef9e92de7e9f375527970f6691a0ade2267f7
heyhoe <h3y6e@pm.me>
2023-11-16 03:26:31 +0900

    add ghq and peco

--- .chezmoiscripts/windows/run_onchange_before_scoop.ps1
+++ .chezmoiscripts/windows/run_onchange_before_scoop.ps1
@@ -2,3 +2,10 @@ if (!(Get-Command scoop -ErrorAction SilentlyContinue)) {
   Write-Host "Installing scoop..."
   Invoke-RestMethod get.scoop.sh | Invoke-Expression
 }
+
+@(
+  "ghq",
+  "peco"
+) | ForEach-Object {
+  scoop install $_
+}
