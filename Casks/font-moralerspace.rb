6d53a0fe3de05983c1ecd23527141218a2e882ea
heyhoe <h3y6e@pm.me>
2024-02-08 01:33:16 +0900

    add sha256

--- Casks/font-moralerspace.rb
+++ Casks/font-moralerspace.rb
@@ -1,5 +1,6 @@
 cask "font-moralerspace" do
   version "0.0.1"
+  sha256 "3d4792ebdc5ff1d302f829314a9558e4e2e04e2260ee73395f26a072733becd1"
 
   url "https://github.com/yuru7/moralerspace/releases/download/v#{version}/moralerspace_v#{version}.zip"
   name "moralerspace"
