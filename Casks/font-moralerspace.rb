03ca60c12b86eddff0214fa69d446f88ee4928f3
heyhoe <h3y6e@pm.me>
2024-02-08 01:50:57 +0900

    m->M

--- Casks/font-moralerspace.rb
+++ Casks/font-moralerspace.rb
@@ -2,17 +2,17 @@ cask "font-moralerspace" do
   version "0.0.1"
   sha256 :no_check
 
-  url "https://github.com/yuru7/moralerspace/releases/download/v#{version}/moralerspace_v#{version}.zip"
+  url "https://github.com/yuru7/moralerspace/releases/download/v#{version}/Moralerspace_v#{version}.zip"
   name "moralerspace"
   desc "Programming font based on Monaspace and IBM Plex Sans JP"
   homepage "https://github.com/yuru7/moralerspace"
 
-  font "moralerspace_v#{version}/MoralerspaceNeon-Bold.ttf"
-  font "moralerspace_v#{version}/MoralerspaceNeon-BoldItalic.ttf"
-  font "moralerspace_v#{version}/MoralerspaceNeon-Italic.ttf"
-  font "moralerspace_v#{version}/MoralerspaceNeon-Regular.ttf"
-  font "moralerspace_v#{version}/MoralerspaceArgon-Bold.ttf"
-  font "moralerspace_v#{version}/MoralerspaceArgon-BoldItalic.ttf"
-  font "moralerspace_v#{version}/MoralerspaceArgon-Italic.ttf"
-  font "moralerspace_v#{version}/MoralerspaceArgon-Regular.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceNeon-Bold.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceNeon-BoldItalic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceNeon-Italic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceNeon-Regular.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceArgon-Bold.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceArgon-BoldItalic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceArgon-Italic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceArgon-Regular.ttf"
 end
