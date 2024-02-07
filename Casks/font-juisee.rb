54175b230d95f7d4532c97a6bfeb866a53473737
heyhoe <h3y6e@pm.me>
2024-02-08 01:56:09 +0900

    update juisee version

--- Casks/font-juisee.rb
+++ Casks/font-juisee.rb
@@ -1,6 +1,6 @@
 cask "font-juisee" do
-  version "0.0.4"
-  sha256 "36f7bac0e89d76719d6948fc042721331e22c5a683c5f10de7ddb0ff12c2dde1"
+  version "0.0.6"
+  sha256 :no_check
 
   url "https://github.com/yuru7/juisee/releases/download/v#{version}/Juisee_v#{version}.zip"
   name "Juisee"
