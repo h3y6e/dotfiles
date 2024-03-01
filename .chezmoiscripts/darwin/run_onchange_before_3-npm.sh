92c84861853f02bb0a0e0497b31b790519910fc3
heyhoe <h3y6e@pm.me>
2024-03-01 15:22:38 +0900

    remove github-copilot-cli

--- .chezmoiscripts/darwin/run_onchange_before_3-npm.sh
+++ .chezmoiscripts/darwin/run_onchange_before_3-npm.sh
@@ -4,12 +4,6 @@ set -Eeufo pipefail
 
 printf "\033[3;35m%s\033[m\n" "npm installing..."
 
-if ! type github-copilot-cli &>/dev/null; then
-  echo "installing github-copilot-cli..."
-  npm install -g @githubnext/github-copilot-cli
-  github-copilot-cli auth
-fi
-
 if ! type npm-check &>/dev/null; then
   echo "installing npm-check..."
   npm install -g npm-check
