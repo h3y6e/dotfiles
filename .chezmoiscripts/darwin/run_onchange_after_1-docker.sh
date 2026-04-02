a7c155a38f39a227bb88f82010fda4df1bd1e9e2
heyhoe <h3y6e@pm.me>
2026-04-02 18:08:25 +0900

    fix(docker): Fix docker config script to handle missing config file

--- .chezmoiscripts/darwin/run_onchange_after_1-docker.sh
+++ .chezmoiscripts/darwin/run_onchange_after_1-docker.sh
@@ -4,7 +4,10 @@ set -Eeufo pipefail
 
 CONFIG_PATH="${HOME}/.docker/config.json"
 
-if [[ -f "$CONFIG_PATH" ]] && command -v jq > /dev/null; then
+if command -v jq > /dev/null; then
   printf "\033[3;35m%s\033[m\n" "setting up docker..."
-  jq '.["cliPluginsExtraDirs"] |= (.+["/opt/homebrew/lib/docker/cli-plugins"] | unique)' "$CONFIG_PATH" > "${CONFIG_PATH}.tmp" && mv "${CONFIG_PATH}.tmp" "$CONFIG_PATH"
+  mkdir -p "$(dirname "$CONFIG_PATH")"
+  (cat "$CONFIG_PATH" 2>/dev/null || echo '{}') \
+    | jq '.cliPluginsExtraDirs |= (. + ["/opt/homebrew/lib/docker/cli-plugins"] | unique)' \
+    > "${CONFIG_PATH}.tmp" && mv "${CONFIG_PATH}.tmp" "$CONFIG_PATH"
 fi
