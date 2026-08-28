cc8ed35e108b91dffbe9b58310b539fc1b4a3525
heyhoe <h3y6e@pm.me>
2026-08-28 10:20:04 +0900

    fix(mise): enable automatic updates

--- .chezmoiscripts/darwin/run_after_3-up.sh
+++ .chezmoiscripts/darwin/run_after_3-up.sh
@@ -5,7 +5,6 @@ set -Eeufo pipefail
 export PATH="${HOME}/.local/bin:${PATH}"
 
 printf "\033[3;35m%s\033[m\n" "mise updating..."
-mise self-update -y
 mise upgrade
 mise bootstrap packages upgrade --yes
 mise x -- sheldon lock --update
