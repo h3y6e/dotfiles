468b5d76d61eadd3e42845864d19117174878334
heyhoe <h3y6e@pm.me>
2026-08-28 12:04:30 +0900

    fix(darwin): remove mise update status message

--- .chezmoiscripts/darwin/run_after_3-up.sh
+++ .chezmoiscripts/darwin/run_after_3-up.sh
@@ -4,7 +4,6 @@ set -Eeufo pipefail
 
 export PATH="${HOME}/.local/bin:${PATH}"
 
-printf "\033[3;35m%s\033[m\n" "mise updating..."
 mise upgrade
 mise bootstrap packages upgrade --yes
 mise x -- sheldon lock --update
