a022069387aac75829675523c37747c175a70730
heyhoe <h3y6e@pm.me>
2026-09-20 22:15:52 +0900

    fix(mise): use shorthand global skills flag

--- .chezmoiscripts/darwin/run_after_3-up.sh
+++ .chezmoiscripts/darwin/run_after_3-up.sh
@@ -5,7 +5,7 @@ set -Eeufo pipefail
 export PATH="${HOME}/.local/bin:${PATH}"
 
 mise upgrade
-mise skills sync --global
+mise skills sync -g
 mise bootstrap packages upgrade --yes
 mise x -- sheldon lock --update
 
