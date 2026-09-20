6e3b88c5b47d52a12c6bb70f72eb9590531ee88a
heyhoe <h3y6e@pm.me>
2026-09-20 21:36:00 +0900

    fix(mise): Sync and prune skills automatically

--- .chezmoiscripts/darwin/run_after_3-up.sh
+++ .chezmoiscripts/darwin/run_after_3-up.sh
@@ -5,6 +5,7 @@ set -Eeufo pipefail
 export PATH="${HOME}/.local/bin:${PATH}"
 
 mise upgrade
+mise skills sync --global --prune
 mise bootstrap packages upgrade --yes
 mise x -- sheldon lock --update
 
